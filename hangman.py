4c6ba46e537171f76c7cc982dd94999dd95cb48b
Artem Martirosyan <syanches@gmail.com>
2022-08-16 12:35:30 +0300

    my first commit from terminal

---
+++ hangman.py
@@ -0,0 +1,44 @@
+import random
+
+def hangman():
+    list_of_words = ["автомобиль", "паровоз", "ребенок", "сомелье", "книголюб" ,"волкодав" ,"колонка"]
+    word = list_of_words[random.randint(0, (len(list_of_words)-1))]
+    wrong = 0
+    stages = [ "",
+    "______      ",
+    "|           ",
+    "|     |     ",
+    "|     0     ",
+    "|   / | \   ",
+    "|    / \    ",
+    "|            "
+    ]
+    rletters = list(word)
+    board = ["_"] * len(word)
+    win = False
+    print("Добро пожаловать на казнь!")
+
+    while wrong < len(stages) - 1:
+        print("\n")
+        msg = "Введите букву: "
+        char = input(msg)
+        if char in rletters:
+            cind = rletters.index(char)
+            board[cind] = char
+            rletters[cind] = '$'
+        else:
+            wrong += 1
+        print((" ".join(board)))
+        e = wrong + 1
+        print("\n".join(stages[0:e]))
+        if "_" not in board:
+            print("Вы выиграли! Было загадано слово:")
+            print(" ".join(board))
+            win = True
+            break
+
+    if not win:
+        print("\n".join(stages[0: wrong]))
+        print("Вы проиграли! Было загадано слово: {}.".format(word))
+
+hangman()
